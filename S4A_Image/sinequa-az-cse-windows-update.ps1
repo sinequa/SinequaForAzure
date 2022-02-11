bdcb2a2ac66c20dc5925cdc71ba67d557cf8c734
larde <larde@sinequa.com>
2022-02-12 00:21:57 +0100

    more logs

--- S4A_Image/sinequa-az-cse-windows-update.ps1
+++ S4A_Image/sinequa-az-cse-windows-update.ps1
@@ -1,3 +1,18 @@
 #Install-Module PSWindowsUpdate -Force
 #Get-WindowsUpdate -IgnoreReboot
 Install-WindowsUpdate -AcceptAll -Install -IgnoreReboot
+
+$reboot = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing"  -Name "RebootPending" -ErrorAction Ignore
+if (-not $reboot) {
+    $reboot = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update'  -Name 'RebootRequired' -ErrorAction Ignore
+}
+if (-not $reboot) {
+    $res = Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager' -Name 'PendingFileRenameOperations' -ErrorAction Ignore
+    if ($res -and $res.PendingFileRenameOperations) {
+        $reboot = "PendingFileRenameOperations"
+        $res.PendingFileRenameOperations
+    }
+}
+if ($reboot) {
+    Write-Host "Reboot required"
+}
