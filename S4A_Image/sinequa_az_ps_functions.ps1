44466d9944a170a2969d08ea8d7b14c4eef86de5
larde <larde@sinequa.com>
2022-02-14 09:50:07 +0100

    remove manual update

--- S4A_Image/sinequa_az_ps_functions.ps1
+++ S4A_Image/sinequa_az_ps_functions.ps1
@@ -206,7 +206,7 @@ function SqAzurePSCreateVMforNode(
     $vm = New-AzVMConfig -VMName $vmName -VMSize $vmSize -IdentityType SystemAssigned
 
     # Set the VM Size and Type
-    $null = Set-AzVMOperatingSystem -VM $vm -Windows -ComputerName $hostName -Credential $credential -PatchMode Manual -ProvisionVMAgent
+    $null = Set-AzVMOperatingSystem -VM $vm -Windows -ComputerName $hostName -Credential $credential -ProvisionVMAgent
 
     # Enable the provisioning of the VM Agent
     if ($vm.OSProfile.WindowsConfiguration) {
